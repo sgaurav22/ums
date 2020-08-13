33a5693419176b0d8cc48276035a8d06f8d19ca5
Gaurav Saxena <gaurav.saxena@nttdsdwsrnd.com>
2020-08-13 19:19:38 +0530

    Intial Commit

---
+++ src/main/java/com/mav/business/ums/domain/User.java
@@ -0,0 +1,27 @@
+package com.mav.business.ums.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+}
